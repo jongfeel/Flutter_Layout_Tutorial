5ec109346280ed45d9fb5821f25f052e68b8f60c
Kim Jong Feel <DeveloperFEEL@gmail.com>
2025-08-06 23:47:52 +0900

    Position the buttons with a Row widget

--- flutter_application_layout_tutorial/lib/main.dart
+++ flutter_application_layout_tutorial/lib/main.dart
@@ -73,7 +73,16 @@ class ButtonSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Color color = Theme.of(context).primaryColor;
-    return Center();
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonWithText(color: color, icon: Icons.call, label: 'CALL'),
+          ButtonWithText(color: color, icon: Icons.near_me, label: 'ROUTE'),
+          ButtonWithText(color: color, icon: Icons.share, label: 'SHARE'),
+        ],
+      ),
+    );
   }
 
 }
